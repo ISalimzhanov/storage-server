beeacef70b65fc4d63939a914c201b1044b94dbe
ISalimzhanov <iskabaur@gmai..com>
2020-10-09 20:46:58 +0600

    fixed ServerService

--- API/server.py
+++ API/server.py
@@ -16,30 +16,27 @@ class ServerService(object):
     def init(self) -> int:
         return self.storage.available_space()
 
-    def create(self, id: bytes) -> None:
-        path = id.decode('utf-8')
-        self.storage.create(path)
+    def create(self, id: str) -> None:
+        self.storage.create(id)
 
-    def write(self, id: bytes, content: bytes) -> None:
-        path = id.decode('utf-8')
-        self.storage.write(path, content)
+    def write(self, id: str, contents: bytes) -> None:
+        self.storage.write(id, contents)
 
-    def read(self, id: bytes) -> bytes:
-        path = id.decode('utf-8')
-        return self.storage.read(path)
+    def read(self, id: str) -> bytes:
+        return self.storage.read(id)
 
     def delete(self, ids: list) -> None:
         for id in ids:
-            if type(id) != bytes:
+            if type(id) != str:
                 raise ValueError
         for id in ids:
-            path = id.decode('utf-8')
-            self.storage.delete(path)
+            self.storage.delete(id)
 
 
 @app.route('/', methods=['POST'])
-def handler():
-    req_body = msgpack.unpackb(request.get_data(), use_list=False)
+def handler(data: bytes):
+    # req_body = msgpack.unpackb(request.get_data(), use_list=False)
+    req_body = msgpack.unpackb(data, use_list=False)
     ss = ServerService()
     result = None
     try:
@@ -50,11 +47,11 @@ def handler():
         elif req_body['method'] == 'create':
             ss.create(req_body['params']['id'])
         elif req_body['method'] == 'write':
-            ss.create(req_body['params']['id'])
+            ss.write(req_body['params']['id'], req_body['params']['contents'])
         elif req_body['method'] == 'read':
-            result = {'contents': ss.create(req_body['params']['id'])}
+            result = {'contents': ss.read(req_body['params']['id'])}
         elif req_body['method'] == 'delete':
-            ss.delete(req_body['params']['id'])
+            ss.delete(req_body['params']['ids'])
         response = {'jsonrpc': '2.0', 'result': result, 'success': True, 'type': req_body['method']}
     except Exception:
         response = {'jsonrpc': '2.0', 'error': 'something went wrong', 'success': False, 'type': req_body['method']}
