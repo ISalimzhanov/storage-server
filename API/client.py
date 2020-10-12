1a86ea6df40f21c3635730c50f040691525d063d
ISalimzhanov <iskabaur@gmai..com>
2020-10-12 16:20:31 +0600

    fixed Response

--- API/client.py
+++ API/client.py
@@ -1,16 +1,18 @@
 import msgpack
 import requests
 import os
+from flask import Response
 
 
 def register(connector: str) -> bool:
     req = {'jsonrpc': '2.0', 'method': 'replica.register', 'params': {'connector': connector}}
     req_data = msgpack.packb(req)
-    response_data = requests.post(f'http://{os.environ["ns_host"]}:{os.environ["ns_port"]}/api/replica', data=req_data)
-    response = msgpack.unpackb(response_data)
-    if response['success']:
+    response: Response = requests.post(f'http://{os.environ["ns_host"]}:{os.environ["ns_port"]}/api/replica',
+                                       data=req_data)
+    response_data = msgpack.unpackb(response.get_data())
+    if response_data['success']:
         with open('id', 'w') as file:
-            file.write(response['id'])
+            file.write(response_data['id'])
         return True
     return False
 
@@ -20,6 +22,7 @@ def connect() -> bool:
         id = file.read()
     req = {'jsonrpc': '2.0', 'method': 'replica.register', 'params': {'id': id}}
     req_data = msgpack.packb(req)
-    response_data = requests.post(f'http://{os.environ["ns_host"]}:{os.environ["ns_port"]}/api/replica', data=req_data)
-    response = msgpack.unpackb(response_data)
-    return response['success']
+    response: Response = requests.post(f'http://{os.environ["ns_host"]}:{os.environ["ns_port"]}/api/replica',
+                                       data=req_data)
+    response_data = msgpack.unpackb(response.get_data())
+    return response_data['success']
